069290a81be417e4aa616341d599303a6be97c06
supawarin <supawarin8@gmail.com>
2022-11-15 11:21:11 +0700

    Create Helm Chart

--- app/index.js
+++ app/index.js
@@ -2,4 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => res.send('Hello world from kubernetes!'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
+
 //app.listen(80, '0.0.0.0', () => console.log('Example app listening on port 80!'))
